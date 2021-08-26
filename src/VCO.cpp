a74d6add8d44317ee83439903ad2799684a81edc
Daniel Billotte <daniel@litfuze.com>
2021-08-26 13:35:50 -0700

    wrote very simple sine/square oscillator

--- src/VCO.cpp
+++ src/VCO.cpp
@@ -4,12 +4,9 @@
 struct VCO : Module {
 	enum ParamIds {
 		FREQUENCY_PARAM,
-		FREQUENCY_CV_ATTN_PARAM,
 		NUM_PARAMS
 	};
 	enum InputIds {
-		VOCT_INPUT,
-		FREQUENCY_CV_INPUT,
 		NUM_INPUTS
 	};
 	enum OutputIds {
@@ -21,13 +18,54 @@ struct VCO : Module {
 		NUM_LIGHTS
 	};
 
+	float _phase = 0.f;
+	float _gain = 5.f; // this results in signal in [-5V..5V]
+
+
 	VCO() {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
-		configParam(FREQUENCY_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(FREQUENCY_CV_ATTN_PARAM, 0.f, 1.f, 0.f, "");
+
+		// configure the frequency knob or "param":
+		// min-val: 0.0
+		// max-val: 10,000.0
+		// default-val: 440.0
+		// description that shows up in the UI
+		configParam(FREQUENCY_PARAM, 0.f, 10000.f, 440.f, "Frequency in Hz");
 	}
 
+	/*
+	 * This is where your plugin does all of its work.
+	 */
 	void process(const ProcessArgs& args) override {
+		// get the base frequency from the frequency-knob
+		float freq = params[FREQUENCY_PARAM].getValue();
+
+		// calculate the site output 		
+		float sine_output = sin(_phase);
+
+		// write the sine output
+		outputs[SINE_OUTPUT].setVoltage(sine_output * _gain);
+
+		// calculate and write the square output
+		float square_output = (sine_output >= 0) ? 1.f : -1.f;
+		outputs[SQUARE_OUTPUT].setVoltage(square_output * _gain);
+
+		// move the oscillator forward one step
+		stepForward(freq, args.sampleRate);
+	}
+
+	/*
+	 * I added this function just to keep process() short and to the point
+	 */
+	void stepForward(float freq, int sampleRate) {
+		// calculate the phase increment
+		float phase_increment = (2 * M_PI * freq) / sampleRate;
+
+		// push the oscillator forward one step (the phase increment)
+		_phase += phase_increment;
+		if(_phase >= 2 * M_PI) {
+			_phase -= 2 * M_PI;
+		}
 	}
 };
 
@@ -37,19 +75,16 @@ struct VCOWidget : ModuleWidget {
 		setModule(module);
 		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/VCO.svg")));
 
+		// Add the screws to the UI. This is just for looks
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
-		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
-		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(10.792, 19.932)), module, VCO::FREQUENCY_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(10.792, 54.451)), module, VCO::FREQUENCY_CV_ATTN_PARAM));
+		// place a RoundLargeBlackKnob for the frequency knob ("param")
+		addParam(createParamCentered<RoundLargeBlackKnob>(mm2px(Vec(10.792, 30.f)), module, VCO::FREQUENCY_PARAM));
 
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(10.792, 38.597)), module, VCO::VOCT_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(10.792, 65.871)), module, VCO::FREQUENCY_CV_INPUT));
-
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(10.792, 108.329)), module, VCO::SINE_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(10.792, 119.482)), module, VCO::SQUARE_OUTPUT));
+		// place the output jacks
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(10.792, 95.f)), module, VCO::SINE_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(10.792, 120.f)), module, VCO::SQUARE_OUTPUT));
 	}
 };
 
